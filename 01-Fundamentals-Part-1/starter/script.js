fba099a5d3d57c3a7ed5d50ced05131e35794790
O-Jacks <olly.p.jacks@gmail.com>
2021-03-05 18:24:20 +1300

    Add JS for First couple lessons

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+      /*
+      let js = "amazing";
+      console.log(40 + 8 + 23 - 10);
+
+      console.log("Jonas");
+      console.log(23);
+
+      let 3firstName = "Bob";
+      console.log(firstName);
+      
+
+      true;
+      console.log(true);
+      console.log(typeof true);
+      */
+
+      
+
